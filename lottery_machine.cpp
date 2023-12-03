7f87258707ea8e93a1e3b934941d27624749a503
md-whahid <mdislamwhahid@gmail.com>
2023-12-03 23:47:33 +0600

    tab tab fix

--- lottery_machine.cpp
+++ lottery_machine.cpp
@@ -3,17 +3,43 @@ using namespace std;
 
 int main()
 {
-  //   int students[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  //                     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  //                     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-  //                     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-  //                     41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-  //                     51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  //                     61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-  //                     71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-  //                     81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-  //                     91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
-  //                     101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112};
 
+  int student_numbers = 0;
+
+  cout << "Enter the candidate number: ";
+  cin >> student_numbers;
+
+  int students[student_numbers];
+  int selected_numbers[12];
+
+  for (int i = 0; i < student_numbers; i++)
+  {
+    students[i] = i + 1;
+    cout << students[i] << "\n";
+
+    srand(time(NULL));
+
+    int random_roll = (rand() % 10) + 1;
+
+    int condition = student_numbers;
+
+    do
+    {
+      if (random_roll == students[i])
+      {
+        break;
+      }
+      else
+      {
+        selected_numbers[i] = random_roll;
+      }
+      condition--;
+    } while (condition != 0);
+  }
+  int selected_size = sizeof(selected_numbers) / sizeof(int);
+  for (int i = 0; i < selected_size; i++)
+  {
+    cout << ">>>" << selected_numbers[i] << "\n";
+  }
   return 0;
 }
