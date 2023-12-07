6f08c5217fc2e03cbf936667444a09629686c792
md-whahid <mdislamwhahid@gmail.com>
2023-12-07 20:22:34 +0600

    #fix it DonE

--- turbo.cpp
+++ turbo.cpp
@@ -1,65 +1,59 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
+
+void drawBoard(char *boardBox);
+void playerTurn(char *boardBox, char player);
+void computerTurn(char *boardBox, char computer);
+bool checkWinner(char *boardBox, char player, char computer);
+bool checkTie(char *boardBox);
 
 int main()
 {
-  // int num[5] = {1, 2, 3, 4, 5};
-  // int towDnum[3][3] = {
-  //     {10, 20, 30},
-  //     {11, 22, 33},
-  //     {77, 88, 99}};
-  // int size = sizeof(num) / sizeof(num[0]);
-  // cout << size << endl;
-  // cout << num[2] << endl;
-  // cout << "hello: " << towDnum[0][0];
+  char boardBox[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+  bool game_running = true;
+  char player = 'X';
+  char computer = 'O';
 
-  string questions[] = {
-      "1. What is your name? ",
-      "2. What is your fav color? ",
-      "3. What is your userID? ",
-      "4. What is your date of birth? "};
-
-  int size = sizeof(questions) / sizeof(questions[0]);
-
-  string options[][3] = {
-      {"A. Yusuf", "B. Zakir", "C. Whahid"},
-      {"A. Red", "B. Green", "C. Orenge"},
-      {"A. whahid-sd", "B. whahid12", "C. md-whahid"},
-      {"A. 14", "B. 63", "C. 12"}};
-
-  char answers[] = {'C', 'B', 'C', 'A'};
-  char gess;
-  int count = 0;
-  double score = 0;
-
-  for (int i = 0; i < size; i++)
+  while (game_running)
   {
-    cout << "\n***   ***   ***\n";
-    cout << questions[i] << "\n";
-    cout << "***   ***   ***\n";
-
-    for (int j = 0; j < sizeof(options[i]) / sizeof(options[i][0]); j++)
-    {
-      cout << options[i][j] << "\n";
-    }
-    cin >> gess;
-    gess = toupper(gess);
-    if (gess == answers[i])
-    {
-      cout << "::: Correct :::";
-      cout << "\n***   ***   ***\n";
-      count++;
-    }
-    else
-    {
-      cout << "!!! Worng !!!\n";
-      cout << "Correct answer: >>> " << answers[i] << " <<<\n";
-      cout << "***   ***   ***\n";
-    }
+    drawBoard(boardBox);
+    playerTurn(boardBox, player);
+    computerTurn(boardBox, computer);
   }
-  score = (count / (double)size) * 100;
-  cout << "You got " << count << " questions correct out of " << size << " questions.\n";
-  cout << "Your accuracy score is: " << score << "%";
 
+  return 0;
+}
+
+void drawBoard(char *boardBox)
+{
+  cout << "     |     |     \n";
+  cout << "  " << boardBox[0] << "  |  " << boardBox[1] << "  |  " << boardBox[2] << "  \n";
+  cout << "_____|_____|_____\n";
+  cout << "     |     |     \n";
+  cout << "  " << boardBox[3] << "  |  " << boardBox[4] << "  |  " << boardBox[5] << "  \n";
+  cout << "_____|_____|_____\n";
+  cout << "     |     |     \n";
+  cout << "  " << boardBox[6] << "  |  " << boardBox[7] << "  |  " << boardBox[8] << "  \n";
+  cout << "     |     |     \n";
+}
+void playerTurn(char *boardBox, char player)
+{
+  int choise = 0;
+  cout << "Enter Your Choise (1-9): ";
+  cin >> choise;
+  choise -= 1;
+  boardBox[choise] = player;
+}
+void computerTurn(char *boardBox, char computer)
+{
+  srand(time(NULL));
+}
+bool checkWinner(char *boardBox, char player, char computer)
+{
+  return 0;
+}
+bool checkTie(char *boardBox)
+{
   return 0;
 }
